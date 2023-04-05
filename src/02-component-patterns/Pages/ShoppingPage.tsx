9b9b5f3d3d6e1d80f7c451bc3ae8b3bf9e9543cb
AlvaroRuiz1126 <cheruiz.1126@gmail.com>
2023-04-04 22:31:50 -0500

    pre npm package

--- src/02-component-patterns/Pages/ShoppingPage.tsx
+++ src/02-component-patterns/Pages/ShoppingPage.tsx
@@ -6,12 +6,6 @@ import {
 } from "../components/index";
 import { products } from "../data/products";
 import "../style/custom-styles.css";
-/* import {
-  //ProductButtons,
-  ProductCard,
-  //ProductImage,
-  //ProductTitle,
-} from "../components/ProductCard"; */
 
 const product = products[0];
 
@@ -22,21 +16,14 @@ const ShoppingPage = () => {
       <hr />
       <ProductCard
         product={product}
-        className="bg-dark"
         key={product.id}
         initialValues={{ count: 4, maxCount: 10 }}
       >
         {({ reset, isMaxCountReach, count, increaseBy }) => (
           <>
-            <ProductImage className="custom-image" />
-            <ProductTitle className="text-white" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {!isMaxCountReach && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-            <span>{count}</span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
